b618c3e5b79bdb7a6d2148aed510bbd45ab3a239
rahuljuit <denre4@gmail.com>
2020-09-08 15:55:50 +0530

    Deploying all files

---
+++ main.js
@@ -0,0 +1,64 @@
+function f1()
+		{
+			document.Calculator.ans.value +="1"
+		};
+		function f2()
+		{
+			document.Calculator.ans.value +="2"
+		};
+		function f3()
+		{
+			document.Calculator.ans.value +="3"
+		};
+		function f4()
+		{
+			document.Calculator.ans.value +="4"
+		};
+		function f5()
+		{
+			document.Calculator.ans.value +="5"
+		};
+		function f6()
+		{
+			document.Calculator.ans.value +="6"
+		};
+		function f7()
+		{
+			document.Calculator.ans.value +="7"
+		};
+		function f8()
+		{
+			document.Calculator.ans.value +="8"
+		};
+		function f9()
+		{
+			document.Calculator.ans.value +="9"
+		};
+		function f0()
+		{
+			document.Calculator.ans.value +="0"
+		};
+		function fplus()
+		{
+			document.Calculator.ans.value +="+"
+		};
+		function fminus()
+		{
+			document.Calculator.ans.value +="-"
+		};
+		function fproduct()
+		{
+			document.Calculator.ans.value +="*"
+		};
+		function fdivide()
+		{
+			document.Calculator.ans.value +="/"
+		};
+		function runC()
+		{
+			document.Calculator.ans.value +="."
+		};
+		function fEquals() {
+	var equals = eval(document.Calculator.ans.value)
+	document.Calculator.ans.value = equals;
+}
